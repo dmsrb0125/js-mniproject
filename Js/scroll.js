2fe9f692d8e636c94e1993c740db055b696b210d
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-04-08 20:26:24 +0900

    refactor: 병합 파일 충돌 삭제

--- Js/scroll.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-  const navbar = document.querySelector(".navbar");
-  // 스크롤 위치가 50px 이상일 때 'scrolled' 클래스를 추가하고, 그렇지 않으면 제거합니다.
-  if (window.scrollY > 50) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
